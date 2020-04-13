5c0e90741a6f51cd6d8f329ed187e3a3543ccf33
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-13 08:02:32 -0400

    src/defconfig.sh: update

--- src/defconfig.sh
+++ src/defconfig.sh
@@ -7,7 +7,7 @@ defconfig() {
     showinfo get_project_name PROJECT
     showinfo get_user         USER
     showinfo get_commit_count COMMITS
-    showinfo get_head         HEAD
+    showinfo get_head_long    HEAD
     showinfo get_created      CREATED
     showinfo get_srcsize      SIZE
     printf '\n'
