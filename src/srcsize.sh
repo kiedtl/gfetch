27eebbbaa71b8001bba8129dccf922dbfd1541d7
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-13 11:30:00 -0400

    srcsize: check if file was deleted before counting size

--- src/srcsize.sh
+++ src/srcsize.sh
@@ -9,6 +9,12 @@ get_srcsize() {
     # retrieving the size with du -sb.
     for file in $(git ls-files)
     do
+        # just in case the user removed a file
+        # but didn't commit the removal yet,
+        # in that case git still lists the deleted
+        # file. ¯\_(ツ)_/¯
+        [ -f "$file" ] || continue
+
         size=$((size+=$(du -sb "$file" | awk '{ print $1 }')))
     done
 
