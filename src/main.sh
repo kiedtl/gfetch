b8dcd3cf884c2fc8b64de89c7f5330094545778c
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-14 09:48:12 -0400

    main.sh: update

--- src/main.sh
+++ src/main.sh
@@ -7,7 +7,16 @@ main() {
         cd "$GFE_DIR"
     fi
 
-    defconfig
+    # check if configuration file exists, otherwise
+    # write default configuration.
+    configdir="${XDG_CONFIG_HOME}/gfe"
+    [ ! -d "$configdir" ] && mkdir -p "$configdir"
+    config="$configdir/config.sh"
+    [ ! -f "$config" ] && printf "$defconfig" > "$config"
+
+    # execute configuration
+    source "$config"
+    gfe_info
 }
 
 main "$@"
