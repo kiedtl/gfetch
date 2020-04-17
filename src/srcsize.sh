70ab7e4620f708391e9c3b8cd4f79176aa57060e
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-04-17 13:58:29 -0400

    optimize srcsize

--- src/srcsize.sh
+++ src/srcsize.sh
@@ -3,25 +3,15 @@
 # See the COPYING file for copyright information.
 
 get_srcsize() {
-    size=0
-    files=0
+    # get a list of files that are tracked by git.
+    files="$(git ls-files)"
+    count="$(echo "$files" | wc -l)"
 
-    # find each file tracked by git,
-    # retrieving the size with du -sb.
-    for file in $(git ls-files)
-    do
-        # just in case the user removed a file
-        # but didn't commit the removal yet,
-        # in that case git still lists the deleted
-        # file. ¯\_(ツ)_/¯
-        [ -f "$file" ] || continue
-
-        size=$((size+=$(du -sb "$file" | awk '{ print $1 }')))
-        files="$((files+1))"
-    done
+    size="$(du -sb $files 2>/dev/null | \
+        awk '{ sz+=$1 } END { print sz }')"
 
     # convert to human-readable format.
     hsize=$(echo "$size" | human)
 
-    printf '%s (%s files)' "$hsize" "$files"
+    printf '%s (%s files)' "$hsize" "$count"
 }
