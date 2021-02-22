965bea960523409e97c496437f6cc2c55b292293
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2021-02-22 22:30:49 +0900

    why not xargs?

--- tests/main.sh
+++ tests/main.sh
@@ -11,11 +11,10 @@ test_command "command -v shellcheck 2>/dev/null >&2" \
     "Shellcheck installed"
 
 header "linting source files"
-for file in src/*
-do
-    test_command "shellcheck -e2148 $file" \
-        "Passed shellcheck: '$file'"
-done
+ls -1 src/ |
+xargs -I file \
+    test_command "shellcheck -e2148 file" \
+    "Passed shellcheck: 'file'"
 
 header "linting tests files"
 test_command "shellcheck tests/main.sh tests/lib.sh" \
